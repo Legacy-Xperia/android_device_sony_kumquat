1af7060e90385ba7cb2104ad314c0ee0b3314a58
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2014-03-08 11:05:47 +0530

    Enable Sony apps in play store[2]

--- cm.mk
+++ cm.mk
@@ -6,5 +6,8 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+#Sony Apps in play store
+PRODUCT_GMS_CLIENTID_BASE := android-sonyericsson
+
 PRODUCT_NAME := cm_kumquat
 PRODUCT_DEVICE := kumquat
